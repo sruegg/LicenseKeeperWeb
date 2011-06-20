52fa7ebed211559860965cb20cf78d516f46c95f
Sebastien Ruegg <sebastien.ruegg@gmail.com>
2011-06-20 22:08:28 +0200

    ...

--- src/web/LoginAction.java
+++ src/web/LoginAction.java
@@ -8,12 +8,23 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public class LoginAction extends Action {
 
-  public ActionForward execute(ActionMapping mapping,
-      ActionForm form, HttpServletRequest request,
-      HttpServletResponse response) {
+public class LoginAction extends Action
+{
+  public ActionForward execute(
+    ActionMapping mapping,
+    ActionForm form,
+    HttpServletRequest request,
+    HttpServletResponse response) throws Exception{
 
-    return mapping.getInputForward();
+	  LoginForm loginform = (LoginForm) form;
+       if (loginform.getUserName().equals("username") && loginform.getPassword().equals("password")) {
+           
+           return mapping.findForward("success");
+       } else {
+          
+           return mapping.findForward("failure");
+       }
   }
 }
+
---
+++ src/web/LoginForm.java
@@ -0,0 +1,26 @@
+package web;
+
+import org.apache.struts.action.ActionForm;
+
+public class LoginForm extends ActionForm {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String userName;
+	private String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
